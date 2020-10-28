29cfc136bbe15c19bbb7a3e410fc9af1d391343c
alexisyhuang <72967007+alexisyhuang@users.noreply.github.com>
2020-10-28 16:07:20 +0800

    Create choices.cpp

---
+++ choices.cpp
@@ -0,0 +1,9 @@
+// file where choices for randomization are held
+#include <bits/stdc++.h>
+using namespace std;
+/*
+Two choices for type of animal: cat and dog
+Two choices for type of door: steel and wooden
+*/
+int animal [2] = {cat, dog};
+int door [2] = {steel, wooden};
