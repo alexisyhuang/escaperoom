5e6ae6a69766a78fc87ed09df25487fbaec43991
Cheung Dawn <u3558426@connect.hku.hk>
2020-11-14 10:42:59 +0800

    delete the old unapplicable scripts

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 using namespace std;
 int main(){
+
+  /** 
   string room[6][2][5] = {};
   // 6 possible locations: door, table, floor, shelf, wall, ceiling
   // each location has 2 "attribute": what other items it contains, other attribute
@@ -11,9 +13,6 @@ int main(){
   // possible items: door, chest, animal, food, key,
   // attribute: identity, adj(wooden/steel; cat/dog), exist?, visible?, getable?, usable?, location,
   randgame(room, items,1);
-
-  for (int i =0; i<6; i++){
-    cout<< room[i]<<endl;
-  }
+  **/
 
 }
--- randgame.cpp
+++ randgame.cpp
@@ -3,8 +3,9 @@
 #include <iostream>
 #include <string>
 using namespace std;
+/** 
 void randgame(string room[6][2][5], string items[5][7], int seed){
-  srand(seed);
+   srand(seed);
   for (int i =0; i<6; i++){
     for (int j =0; j<2; j++){
       for (int k =0; k<5; k++){
@@ -19,5 +20,5 @@ void randgame(string room[6][2][5], string items[5][7], int seed){
       cout<< items[i][j]<<' ';
     }
     cout <<endl;
-  }
+  }**/
 }
