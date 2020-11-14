7159c6fabc4a215df5c29aa1fcb1174b5c7328ef
Cheung Dawn <u3558426@connect.hku.hk>
2020-11-14 13:44:07 +0800

    add chatbot.cpp

--- ask.cpp
+++ ask.cpp
@@ -3,7 +3,47 @@
 #include <sstream>
 #include <algorithm>
 using namespace std;
-const string verb[4]={"eat","get","look","hit"};
+struct myitem{// can add more if you wish
+    string identity;
+    string subtype;
+    int getable; // 1
+    int inPossession;
+    string location;
+};
+int printallitem(int, myitem[]);
+
+int main(){
+
+  int all_items_length = 2;
+  myitem all_items[10]={
+    {"key","silver key", 1, 0, "cat"},
+    {"food","cake",1,0,"table"}
+  };
+
+  printallitem(all_items_length,all_items);
+}
+
+int printallitem(int all_items_length, myitem all_items[  ]){
+  for(int i=0; i < all_items_length; i++){
+    cout<< "identity: "<< all_items[i].identity<<endl
+    <<"subtype: "<<all_items[i].subtype<<endl
+    <<"getable: "<< all_items[i].getable<<endl
+    <<"inPossession: "<< all_items[i].inPossession<< endl
+    <<"location: "<< all_items[i].location<<endl<<endl;
+  }
+}
+/**
+string verb[4]={"eat","get","look","hit"};
+int verblength = 4;
+void chatbot(string, )
+
+**/
+
+
+
+
+/**
+string verb[4]={"eat","get","look","hit"};
 int verblength = 4;
 
 int findverb(int length, string uinputA[]);
@@ -22,7 +62,7 @@ int main(){
     for (int i=0; i<uinputlength; i++){
       cout<< uinputA[i]<<endl;
   }// by this point, user input is stored in an array of words.
-  string actionA[4]={};/**action array: 0= subject(omittable), 1= verb, 2= object**/
+  string actionA[4]={};/**action array: 0= subject(omittable), 1= verb, 2= object
   findverb(uinputlength,uinputA);
   cout << actionA[1];
   }
@@ -38,3 +78,4 @@ int findverb(int length, string uinputA[]){
     }
   }
 }
+**/
