e74e98410ec429912082fca2ccd4d90faa619080
Alexis Huang <72967007+alexisyhuang@users.noreply.github.com>
2020-11-15 01:31:23 +0800

    Added random room generation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include "randgame.h"
+//#include "randgame.h"
 #include <iostream>
-#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 struct myitem{// can add more if you wish
@@ -12,23 +12,64 @@ struct myitem{// can add more if you wish
 };
 
 int main(){
-    int all_items[10];
-    bool savefile;
+    myitem all_items[10];
+    int i = 5;
+    string possibleItems[2][2] = {{"steel", "wooden"}, {"cat", "dog"}};
+    bool savefile = false;
     if (savefile == false) {
       //initialise room
-      int range = 2;
-      int num = rand() % range;
-      new myitem animal;
-      animal.identity = all_items[1][num];
-      new myitem door;
-      door.identity = allitems[0][num];
+      srand(time(NULL));
+      int num1 = rand() % 2;
+      int num2 = rand() % 2;
+      struct myitem animal;
+      animal.identity = possibleItems[1][num1];
+      animal.getable = 0;
+      animal.inPossession = 0;
+      animal.location = "floor";
+      
+      struct myitem door;
+      door.identity = possibleItems[0][num2];
+      door.getable = 0;
+      door.inPossession = 0;
+      door.location = "on the wall";
+        
+      struct myitem specialItem;
+      if (animal.identity == "cat") {
+          specialItem.identity = "fishtank";
+          specialItem.getable = 1;
+          specialItem.inPossession = 0;
+          specialItem.location = "table";
+        } else {
+            specialItem.identity = "turkey leg";
+            specialItem.getable = 1;
+            specialItem.inPossession = 0;
+            specialItem.location = "table";
+        }
+        all_items[0] = door;
+        all_items[1] = animal;
+        all_items[2] = specialItem;
+    } else {
+        cout << "figure this out " << endl;
     }
     //story segment
-  
-  
+    cout << i;
+    cout << all_items[0].identity << endl;
+    cout << all_items[1].identity << endl;
     cout << "Let the story begin..." << endl;
-    cout << "You awaken in a small room, having no idea how you 
-    //I DID THE DATA STRUCTURE PART SO THERES STH TO REFERENCE ON WHEN WORKING WITH THE CHATBOT.  ~daw
+    cout << "You awaken in a small room, your last memory having been peacefully snuggling into bed after a long night of working on your programming assignment." << endl;
+    cout << "A quick glance across the room reveals a " << all_items[0].identity << " door." << endl;
+    if (all_items[0].identity == "wooden") {
+        cout << "It looks weak enough that you might be able to kick it down, if you try hard enough..." << endl;
+    } else {
+        cout << "It looks pretty strong, but maybe you could kick it down if you tried hard enough." << endl;
+    }
+    cout << "Luckily, there's a number pad on the door which you could use to make your escape. You need to get home soon to submit that programming assignment, after all." << endl;
+    cout << "You see a table across the room, and there's also a " << all_items[1].identity << " staring at you judgingly as it sits by a large treasure chest." << endl;
+    cout << "Examine and interact with the objects in the room to make your escape!" << endl;
+    
+    //I DID THE DATA STRUCTURE PART SO THERES STH TO REFERENCE ON WHEN WORKING WITH THE CHATBOT.  ~dawn
+    
+    /*
     int all_items_length = 2;
     myitem all_items[10]={
       {"key","silver key", 1, 0, "cat"},
@@ -41,7 +82,7 @@ int main(){
       <<"inPossession: "<< all_items[i].inPossession<< endl
       <<"location: "<< all_items[i].location<<endl<<endl;
     }
-  }
+  }*/
 
   /**old obsolete scripts see if it's useful or not. if not, just delete it
   string room[6][2][5] = {};
