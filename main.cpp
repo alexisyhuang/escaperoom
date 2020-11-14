3b0b581fe7cf12ed264ed167e5c39521f3f58874
Cheung Dawn <u3558426@connect.hku.hk>
2020-11-14 13:23:40 +0800

    created the data structure

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include "randgame.h"
 #include <iostream>
 using namespace std;
-int main(){
+int main(){//I DID THE DATA STRUCTURE PART SO THERES STH TO REFERENCE ON WHEN WORKING WITH THE CHATBOT.  ~dawn
+  struct myitem{// can add more if you wish
+    string identity;
+    string subtype;
+    int getable; // 1
+  	int inPossession;
+    string location;
+  };
+  int main(){
+    int all_items_length = 2;
+    myitem all_items[10]={
+      {"key","silver key", 1, 0, "cat"},
+      {"food","cake",1,0,"table"}
+    };
+    for(int i=0; i < all_items_length; i++){
+      cout<< "identity: "<< all_items[i].identity<<endl
+      <<"subtype: "<<all_items[i].subtype<<endl
+      <<"getable: "<< all_items[i].getable<<endl
+      <<"inPossession: "<< all_items[i].inPossession<< endl
+      <<"location: "<< all_items[i].location<<endl<<endl;
+    }
+  }
 
-  /** 
+  /**old obsolete scripts see if it's useful or not. if not, just delete it
   string room[6][2][5] = {};
   // 6 possible locations: door, table, floor, shelf, wall, ceiling
   // each location has 2 "attribute": what other items it contains, other attribute
