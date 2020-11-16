8f4cc9670473b11734549264d0146d7193fb0c2d
Cheung Dawn <u3558426@connect.hku.hk>
2020-11-16 17:57:08 +0800

    delete obsolete files

--- choices.cpp
+++
@@ -1,9 +0,0 @@
-// file where choices for randomization will be held
-#include <bits/stdc++.h>
-using namespace std;
-/*
-Two choices for type of animal: cat and dog
-Two choices for type of door: steel and wooden
-*/
-string animals[2] = {"cat", "dog"};
-string door[2] = {"steel", "wooden"};
--- randgame.h
+++
@@ -1,8 +0,0 @@
-#include <string>
-using namespace std;
-#ifndef randgame_h
-#define randgame_h
-
-void randgame(string [6][2][5], string [5][7], int);
-
-#endif
