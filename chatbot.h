d2053d539bd192b9f65766a80ca40654d076d838
Cheung Dawn <u3558426@connect.hku.hk>
2020-11-18 10:07:47 +0800

    update chatbot.h

--- chatbot.h
+++ chatbot.h
@@ -2,6 +2,8 @@
 using namespace std;
 #ifndef chatbot_h
 #define chatbot_h
+//main.cpp function
+int init();
 //chatbot main function
 int chatbot(myitem[],int);
 //chatbot other function
@@ -9,6 +11,7 @@ int getobj(string, myitem[],int);
 int help();
 int hint();
 int invo(myitem[],int);
-int printallitem(int, myitem[]);
+int kick(string, myitem[] ,int);
+int printallitem(myitem[],int);
 int savegame();
 #endif
