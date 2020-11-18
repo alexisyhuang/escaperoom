4bfb25db5081412a3b49697cc56e7dd311a16abd
Cheung Dawn <u3558426@connect.hku.hk>
2020-11-18 10:07:56 +0800

    update structure.h

--- structure.h
+++ structure.h
@@ -7,7 +7,7 @@ struct myitem{// can add more if you wish
     string catagory;//constant
     string identity;// constant
     //int exist;//whether it exist/still exist in this game
-    int getable; //0or1 depends on object property, and presence of obstacles
+    int getable; //0or1 depends on object property, and presence of obstacles //1=recall main
     int knowExistence;//initially setted to 0, becomes 1 when user sees it
     //if getable && knowexistance --> can be picked up
     int inPossession;//in inventory = 1, not in= 0
